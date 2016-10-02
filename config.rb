9625939e6ba8e943403fb2d94070a613ac23b96f
Mike Rivera <rivera.mj@gmail.com>
2016-10-02 11:01:13 -0400

    add neat framework

---
+++ config.rb
@@ -0,0 +1,8 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "src/main/webapp/stylesheets"
+sass_dir = "src/main/webapp/compass-hidden"
+images_dir = "src/main/webapp/images"
+javascripts_dir = "src/main/webapp/js"
